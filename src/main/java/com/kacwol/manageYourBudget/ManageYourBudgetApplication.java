1f53607bcbb75fbed47c312e79b43e913d9337b1
Kacper Wolinski <kacperwolinski404@gmail.com>
2022-06-16 14:18:57 +0200

    Initial commit

---
+++ src/main/java/com/kacwol/manageYourBudget/ManageYourBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.kacwol.manageYourBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageYourBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageYourBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kacwol/manageYourBudget/ManageYourBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacwol.manageYourBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageYourBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
